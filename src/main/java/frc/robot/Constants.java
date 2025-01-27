942d236df715563e6c3f8b87e93db254e6ad693d
Gryphon Robotics <firstteam5549@gmail.com>
2025-01-27 15:47:43 -0500

    Drivetrain

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,7 +16,92 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
+  public static final int DRIVE_CONTROLLER = 0;
+  public static final int OPERATOR_CONTROLLER = 1;
+
+  public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 2; 
+  public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 3;
+  public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 13; 
+
+  public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 4;  
+  public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 5;
+  public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 10; 
+
+  public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 8; 
+  public static final int BACK_LEFT_MODULE_STEER_MOTOR = 9; 
+  public static final int BACK_LEFT_MODULE_STEER_ENCODER = 11; 
+
+  public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 6; 
+  public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 7; 
+  public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12;  
+
+  // Define the conventional order of our modules when putting them into arrays
+  public static final int FRONT_LEFT =0;
+  public static final int FRONT_RIGHT =1;
+  public static final int REAR_LEFT =2;
+  public static final int REAR_RIGHT =3;
+
+  public static final boolean kFrontLeftDriveEncoderReversed = false;
+  public static final boolean kFrontRightDriveEncoderReversed = false;
+  public static final boolean kRearLeftDriveEncoderReversed = true;
+  public static final boolean kRearRightDriveEncoderReversed = true;
+
+  public static final boolean kFrontLeftTurningEncoderReversed = false;
+  public static final boolean kFrontRightTurningEncoderReversed = false;
+  public static final boolean kRearLeftTurningEncoderReversed = true;
+  public static final boolean kRearRightTurningEncoderReversed = true;
+
+  public static final double kWheelDiameterMeters = 0.1016; //0.098; // 0.09398; // 3.7 in
+
+  // The drive encoder reports in RPM by default. Calculate the conversion factor
+  // to make it report in meters per second.
+  public static final double kDriveGearRatio = 8.143;
+  public static final double kDriveConversionFactor = (kWheelDiameterMeters * Math.PI) / kDriveGearRatio;
+
+  public static final double kTurnPositionConversionFactor = 12.8;
+
+  public static final double kMaxSpeedMetersPerSecond = 4;
+  // Units are meters.
+  // Distance between centers of right and left wheels on robot
+  public static final double kTrackWidth = 0.56515;
+  
+  // Distance between front and back wheels on robot
+  public static final double kWheelBase = 0.56515;
+
+  // Units are meters per second
+  public static final double kMaxTranslationalVelocity = 6784 / 60.0 *
+  (1/kDriveGearRatio) *
+  kWheelDiameterMeters * Math.PI;
+
+  // Units are radians per second
+  public static final double kMaxRotationalVelocity = kMaxTranslationalVelocity /
+  Math.hypot(kTrackWidth / 2.0, kWheelBase / 2.0);; //max 5.0
+
+  //The locations f
+  //*or the modules must be relative to the center of the robot. 
+  // Positive x values represent moving toward the front of the robot 
+  // Positive y values represent moving toward the left of the robot.
+  public static final SwerveDriveKinematics kDriveKinematics =
+  new SwerveDriveKinematics(
+      new Translation2d(kWheelBase / 2.0, kTrackWidth / 2.0),   // front left
+      new Translation2d(kWheelBase / 2.0, -kTrackWidth / 2.0),  // front right
+      new Translation2d(-kWheelBase / 2.0, kTrackWidth / 2.0),  // rear left
+      new Translation2d(-kWheelBase / 2.0, -kTrackWidth / 2.0)  // rear right
+      );
+
+
+      
+  public static final boolean kGyroReversed = false;
+
+  public static final double kDriveP = 0.05;
+  public static final double kDriveI = 0.0;
+  public static final double kDriveD = 0.0;
+  public static final double kDriveFF = 0.1;
+
+  public static final double kTurningP = 0.005;
+  public static final double kTurningI = 0.0;
+  public static final double kTurningD = 0.0;
+  public static final double kAcceleration = 4;
+
+  public static final int RESET_NAVX_BUTTON = 4;
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,17 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.DriveCommand;
+import frc.robot.subsystems.DrivetrainSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,14 +23,17 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController m_controller = new CommandXboxController(Constants.DRIVE_CONTROLLER);
+  private final CommandXboxController m_controller2 = new CommandXboxController(Constants.OPERATOR_CONTROLLER);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  //region Subsystems
+  private final AHRS m_ahrs = new AHRS(NavXComType.kMXP_SPI);
+  public final DrivetrainSubsystem m_drive = new DrivetrainSubsystem(m_ahrs);
+  //endregion
+
+  JoystickButton resetNavXButton = new JoystickButton(m_controller.getHID(), Constants.RESET_NAVX_BUTTON);
+
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
@@ -42,13 +49,12 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    // region Drivetrain
+      m_controller.axisGreaterThan(0, 0.07).or(m_controller.axisGreaterThan(1, 0.07)).or(m_controller.axisGreaterThan(4, 0.07))
+      .or(m_controller.axisLessThan(0, -0.07)).or(m_controller.axisLessThan(1, -0.07)).or(m_controller.axisLessThan(4, -0.07))
+      .onTrue(new DriveCommand(m_drive, m_controller));
+      resetNavXButton.onTrue(new InstantCommand(m_drive::zeroGyroscope));
+    //endregion
   }
 
   /**
@@ -58,6 +64,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
   }
 }

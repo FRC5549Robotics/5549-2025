9eba87563ba9c1d3c6bd7c7100af9112cc870d2c
Gryphon Robotics <firstteam5549@gmail.com>
2025-03-07 10:03:13 -0500

    elevator stuff

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "5549-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 38;
-  public static final String GIT_SHA = "515e65c675a0b2d5a83663c007f487e7f0b0a53b";
-  public static final String GIT_DATE = "2025-03-06 14:49:55 EST";
+  public static final int GIT_REVISION = 39;
+  public static final String GIT_SHA = "5420d0438b8b66ca7427079bd2fce4a3199d1b0d";
+  public static final String GIT_DATE = "2025-03-07 09:59:52 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-07 09:57:10 EST";
-  public static final long BUILD_UNIX_TIME = 1741359430301L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-03-07 10:00:15 EST";
+  public static final long BUILD_UNIX_TIME = 1741359615494L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
